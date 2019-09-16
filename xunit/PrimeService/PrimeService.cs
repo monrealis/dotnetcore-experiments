8b164cf36219a44db921887485333c17baee83a3
Vytenis Bivainis <monrealis@gmail.com>
2019-09-16 23:31:34 +0300

    Add xunit hello world
    From https://docs.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-dotnet-test

---
+++ xunit/PrimeService/PrimeService.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Please create a test first.");
+        }
+    }
+}
+

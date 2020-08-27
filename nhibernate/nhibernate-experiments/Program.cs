15fc4f83498a7750a537f241cef605d5944e5eb4
Vytenis Bivainis <monrealis@gmail.com>
2020-08-28 00:18:55 +0300

    Prepare for NHibernate experiments

---
+++ nhibernate/nhibernate-experiments/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nhibernate_experiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
